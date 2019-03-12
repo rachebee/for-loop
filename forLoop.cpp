0e8d48ae8a31496566eea9ee5640e804ca1c25c7
rachebee <46823133+rachebee@users.noreply.github.com>
2019-03-12 14:26:30 -0700

    Add files via upload

---
+++ forLoop.cpp
@@ -0,0 +1,21 @@
+/*Rachel N. Biesiedzinski
+  ForLoop
+  March 12, 2019
+*/
+
+#include <string>
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		cout << al << "\n";
+	}
+
+}
